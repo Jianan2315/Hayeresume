843f7d146a33f6e844893bdf76bfcd2836d42374
Jianan <ni.jian@northeastern.edu>
2024-12-16 09:27:51 -0500

    Update: move login/signup container to center

--- react-client/src/App.jsx
+++ react-client/src/App.jsx
@@ -1,4 +1,4 @@
-// src/App.js
+// src/App.jsx
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './widgets/navbar';
@@ -7,19 +7,24 @@ import Signup from './widgets/signup';
 import UserPortal from './widgets/userPortal';
 import AdminPortal from './widgets/adminPortal';
 import Home from './widgets/home';
+import './App.css';
 
 function App() {
     return (
         <Router>
-            <Navbar />
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/login" element={<Login />} />
-                <Route path="/signup" element={<Signup />} />
-                <Route path="/user" element={<UserPortal />} />
-                <Route path="/admin" element={<AdminPortal />} />
-                <Route path="*" element={<h1>404 - Page Not Found</h1>} />
-            </Routes>
+            <div className="navbar-container">
+                <Navbar />
+            </div>
+            <div className="main-content">
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/signup" element={<Signup />} />
+                    <Route path="/user" element={<UserPortal />} />
+                    <Route path="/admin" element={<AdminPortal />} />
+                    <Route path="*" element={<h1>404 - Page Not Found</h1>} />
+                </Routes>
+            </div>
         </Router>
     );
 }
