f926a1b6ca6f91f933f371c692c2ab834f51118c
Jianan <ni.jian@northeastern.edu>
2024-12-21 05:44:24 -0500

    Improve auth log.

--- api/components/middleware.js
+++ api/components/middleware.js
@@ -1,8 +1,10 @@
 const jwt = require("jsonwebtoken");
 const authenticate = (req, res, next) => {
     const token = req.headers['authorization']?.split(' ')[1];
-    if (!token) return res.status(403).send('Access denied');
-
+    if (!token){
+        console.log('Just double-check if you really need auth for this step.');
+        return res.status(403).send('Access denied');
+    }
     try {
         const decoded = jwt.verify(token, PUBLIC_KEY);
         req.user = decoded;
